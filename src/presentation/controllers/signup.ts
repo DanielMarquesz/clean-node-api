cbac90eb2ebeb8115b610009503e56dbbf7db38a
Daniel Marques <daniel.marques142@gmail.com>
2021-03-08 23:05:06 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -5,12 +5,11 @@ import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   // @ts-expect-error
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('mail'))
+    const requiredFields = ['name', 'mail']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
